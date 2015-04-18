68041adb31e3a2fb915353c9fab2a901e03232c9
NghiaTran <changeyourcolor@gmail.com>
2015-04-18 15:08:47 +0700

    Initial Commit

---
+++ FeRainbowMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FeRainbowMenu
+//
+//  Created by Nghia Tran on 4/18/15.
+//  Copyright (c) 2015 Fe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FeRainbowMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FeRainbowMenu
+//
+//  Created by Nghia Tran on 4/18/15.
+//  Copyright (c) 2015 Fe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
